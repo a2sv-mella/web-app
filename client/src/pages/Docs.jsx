087ba3562017df6a46fc5ef5937cf76fbc8a1596
poseidon2022 <kidusm82@gmail.com>
2024-07-10 15:59:07 +0300

    docs page modified

--- client/src/pages/Docs.jsx
+++ client/src/pages/Docs.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import { HomePageNavbar } from "../components";
 import { forDocs,docCards } from "../utils/constants";
 import DocCard from "../components/DocCard";
+import { Link } from "react-router-dom";
 const Docs = () => {
     const location = useLocation();
 
@@ -13,21 +14,30 @@ const Docs = () => {
         <div className = "mr-10 h-full pr-5">
         
         </div>
-        <div className="flex flex-col text-center justify-center mx-auto w-3/4">
+        <div className="flex flex-col text-center justify-center mx-auto w-[90%]">
           {forDocs.map((cardData, index) => (
             <div className="mb-10">
                 <h3 className="mb-10 font-bold ">{cardData.title}</h3>
-                <d className="text-balance">{cardData.description}</d>
+                <d className="text-balanced">{cardData.description}</d>
             </div>
           ))}
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-10 text-center">
           {docCards.map((cardData, index) => (
-            <DocCard
-              key={index}
-              title={cardData.title}
-              description={cardData.description}
-            />
+            cardData.title === "Quick Start" ? (
+              <Link to="/demo" key={index}>
+                <DocCard
+                  title={cardData.title}
+                  description={cardData.description}
+                />
+              </Link>
+            ) : (
+              <DocCard
+                key={index}
+                title={cardData.title}
+                description={cardData.description}
+              />
+            )
           ))}
         </div>
       </div>
