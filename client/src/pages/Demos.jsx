fee9bd4ba272e640c0cfb0e30677c4cafcbe543e
poseidon2022 <kidusm82@gmail.com>
2024-07-10 15:32:15 +0300

    deleted demo page

--- client/src/pages/Demos.jsx
+++ client/src/pages/Demos.jsx
@@ -1,6 +1,6 @@
 import Wrapper from "../assets/wrappers/LandingPage";
 import { useLocation } from "react-router-dom";
-import { HomePageNavbar, DemoBody } from "../components";
+import { HomePageNavbar} from "../components";
 export default function Demo(){ 
     return (
     <Wrapper>
