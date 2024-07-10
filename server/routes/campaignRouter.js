a9f4326415934784925c465783e52f9e062e1c8d
dododoyo <dolphinmulugeta88@gmail.com>
2024-07-10 17:46:19 +0300

    🔀 Initial Server Routes Setup

---
+++ server/routes/campaignRouter.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const { available, find } = require("../controllers/campaignController.js");
+
+const campaignRouter = Router();
+
+campaignRouter.get("/available", available);
+campaignRouter.get("/:id", find);
+
+module.exports = campaignRouter;
