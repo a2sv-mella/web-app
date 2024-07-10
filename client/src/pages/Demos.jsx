82a71a1dbb45f2f4243418b2d8ca0c71f6873a69
bisratberhanu <bisratbnegus@gmail.com>
2024-07-10 13:56:52 +0300

    ☎️ contact and demo page implemt

--- client/src/pages/Demos.jsx
+++ client/src/pages/Demos.jsx
@@ -1,15 +1,14 @@
 import Wrapper from "../assets/wrappers/LandingPage";
 import { useLocation } from "react-router-dom";
 import { HomePageNavbar } from "../components";
+import DemosBody from "../components/DemosBody"; // Importing the DemosBody component
 const Demos = () => {
   const location = useLocation();
 
   return (
     <Wrapper>
       <HomePageNavbar location={location} />
-      <div className="container page">
-        <h1> Demonstration</h1>
-      </div>
+      <DemosBody /> {/* Rendering the DemosBody component */}
     </Wrapper>
   );
 };
