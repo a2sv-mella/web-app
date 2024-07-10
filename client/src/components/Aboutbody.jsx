7595c8ba76f38cc3e07ada76eb2798a2f588dd03
poseidon2022 <kidusm82@gmail.com>
2024-07-10 12:30:16 +0300

    About Page

--- client/src/components/Aboutbody.jsx
+++ client/src/components/Aboutbody.jsx
@@ -7,7 +7,7 @@ const AboutBody = () => {
       <h2 className="text-3xl font-bold text-center mb-8">
         Behind the Name and Establishment
       </h2>
-      <p className="text-2xl m-auto mb-2"> Why Mella? Simple to connect with the youth through a deeper yet light-spirited humor, it made perfect sense to create a vernacular term for 'Money'. With this intention, the name 'Chapa' was born.</p>
+      <p className="text-2xl m-auto mb-2"> Why Mella? Simple to connect with the youth through a deeper yet light-spirited humor, it made perfect sense to create a vernacular term for 'Money'. With this intention, the name 'Mella' was born.</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {aboutCards.map((cardData, index) => (
           <ServiceCard
